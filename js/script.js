216357ddbecba5933147f4c7dfe15d974426d567
João Faccin <70918083+joaofaccin@users.noreply.github.com>
2023-04-09 20:10:01 -0300

    backgraund muda de acordo com o tipo do pokemon

--- js/script.js
+++ js/script.js
@@ -14,6 +14,8 @@ const input = document.querySelector('.input__search');
 const buttonPrev = document.querySelector('.btn-prev');
 const buttonNext = document.querySelector('.btn-next');
 
+const footer = document.querySelector('.logo');
+
 let searchPokemon = 1;
 
 const fetchPokemon = async (pokemon) => {
@@ -39,6 +41,102 @@ const renderPokemon = async (pokemon) => {
     pokemonHp.innerHTML =  data['stats']['0']['base_stat'];
     pokemonAtk.innerHTML = data['stats']['1']['base_stat'];
     pokemonDef.innerHTML = data['stats']['2']['base_stat'];
+
+    if(pokemontype.innerHTML = data['types']['0']['type']['name']  == 'fire'){
+      var body = document.body;
+      body.style.backgroundRepeat = "no-repeat";
+      body.style.background = "url('./images/fire.jpg')";
+      body.style.backgroundSize = "cover";
+      footer.setAttribute('src', './images/logo-branco.png');
+    }
+    else if(pokemontype.innerHTML = data['types']['0']['type']['name']  == 'water'){
+      var body = document.body;
+      body.style.backgroundRepeat = "no-repeat";
+      body.style.background = "url('./images/agua.jpg')";
+      body.style.backgroundSize = "cover";
+      footer.setAttribute('src', './images/logo-branco.png');
+    }
+    else if(pokemontype.innerHTML = data['types']['0']['type']['name']  == 'grass'){
+      var body = document.body;
+      body.style.backgroundRepeat = "no-repeat";
+      body.style.background = "url('./images/grama.jpg')";
+      body.style.backgroundSize = "cover";
+      footer.setAttribute('src', './images/logo.png');
+    }
+    else if(pokemontype.innerHTML = data['types']['0']['type']['name']  == 'poison'){
+      var body = document.body;
+      body.style.backgroundRepeat = "no-repeat";
+      body.style.background = "url('./images/poison.jpg')";
+      body.style.backgroundSize = "cover";
+      footer.setAttribute('src', './images/logo-branco.png');
+    }
+    else if(pokemontype.innerHTML = data['types']['0']['type']['name']  == 'bug'){
+      var body = document.body;
+      body.style.backgroundRepeat = "no-repeat";
+      body.style.background = "url('./images/bug.jpg')";
+      body.style.backgroundSize = "cover";
+      footer.setAttribute('src', './images/logo.png');
+    }
+    else if(pokemontype.innerHTML = data['types']['0']['type']['name']  == 'normal'){
+      var body = document.body;
+      body.style.backgroundRepeat = "no-repeat";
+      body.style.background = "url('./images/normal.jpg')";
+      body.style.backgroundSize = "cover";
+      footer.setAttribute('src', './images/logo.png');
+    }
+    else if(pokemontype.innerHTML = data['types']['0']['type']['name']  == 'electric'){
+      var body = document.body;
+      body.style.backgroundRepeat = "no-repeat";
+      body.style.background = "url('./images/eletric.png')";
+      body.style.backgroundSize = "cover";
+      footer.setAttribute('src', './images/logo-branco.png');
+    }
+    else if(pokemontype.innerHTML = data['types']['0']['type']['name']  == 'ground'){
+      var body = document.body;
+      body.style.backgroundRepeat = "no-repeat";
+      body.style.background = "url('./images/graund.jpg')";
+      body.style.backgroundSize = "cover";
+      footer.setAttribute('src', './images/logo-branco.png');
+    }
+    else if(pokemontype.innerHTML = data['types']['0']['type']['name']  == 'fairy'){
+      var body = document.body;
+      body.style.backgroundRepeat = "no-repeat";
+      body.style.background = "url('./images/fairy.jpg')";
+      body.style.backgroundSize = "cover";
+      footer.setAttribute('src', './images/logo-branco.png');
+    }
+    else if(pokemontype.innerHTML = data['types']['0']['type']['name']  == 'fighting'){
+      var body = document.body;
+      body.style.backgroundRepeat = "no-repeat";
+      body.style.background = "url('./images/ringbox.jpg')";
+      body.style.backgroundSize = "cover";
+      footer.setAttribute('src', './images/logo-branco.png');
+    }
+    else if(pokemontype.innerHTML = data['types']['0']['type']['name']  == 'psychic'){
+      var body = document.body;
+      body.style.backgroundRepeat = "no-repeat";
+      body.style.background = "url('./images/psychic.jpg')";
+      body.style.backgroundSize = "cover";
+      footer.setAttribute('src', './images/logo-branco.png');
+    }
+    else if(pokemontype.innerHTML = data['types']['0']['type']['name']  == 'ghost'){
+      var body = document.body;
+      body.style.backgroundRepeat = "no-repeat";
+      body.style.background = "url('./images/ghost.jpg')";
+      body.style.backgroundSize = "cover";
+      footer.setAttribute('src', './images/logo-branco.png');
+    }
+    else if(pokemontype.innerHTML = data['types']['0']['type']['name']  == 'dragon'){
+      var body = document.body;
+      body.style.backgroundRepeat = "no-repeat";
+      body.style.background = "url('./images/dragon.jpg')";
+      body.style.backgroundSize = "cover";
+      footer.setAttribute('src', './images/logo-branco.png');
+    }
+    else{
+      document.body.style.background = ""
+      footer.setAttribute('src', './images/logo.png');
+    }
 
     if(pokemontype.innerHTML = data['types']['1'] == null){
       pokemontype.innerHTML = 'Tipo: ' + data['types']['0']['type']['name']    
---
+++ js/teste.js
@@ -0,0 +1,24 @@
+
+
+
+
+// .kicks{
+//     position: absolute;
+//     font-weight: 600;
+//     color: #ffffff;
+//     width: 65%;
+//     top: 150%;
+//     left: 13.5%;
+//     justify-content: space-between;
+//     align-items: center;
+//   }
+//   .pokemon__kick1,
+//   .pokemon__kick2{
+//     width: 50%;
+//     padding: 4%;
+//     border-radius: 5px;
+//     font-size: clamp(8px, 5vw, 1rem);
+//     font-weight: 600;
+//     color: white;
+   
+//   }
